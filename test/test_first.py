453c4d2d71f1a57cbf90d3938e937a512ffc8825
Ilya Slynko <ilya.slynko1@gmail.com>
2022-04-06 16:37:34 +0300

    added G-quadruplex generator in TestFirst

--- test/test_first.py
+++ test/test_first.py
@@ -1,9 +1,33 @@
+import random
 import unittest
+import rstr
+import pprint
 import dafna
+
 
 class TestFirst(unittest.TestCase):
     def test_01(self):
-        ctx = dafna.Context()
+        generator = range(2, 21)
 
-        X = ctx.create_pattern("a|c|g|t", simple=True, name="X")
-        self.assertEqual(3, dafna.max_gquadruplex_strength('TTGGATCTGAGAATCAGATGTGGGTGGGTGGGT'.lower()))
+        def gqd_random_string(g_count):
+            y = '(a|c|t){0,4}'
+            x = '(a|c|t|g)'
+            g_group_size = "Y*".join(['g'] * g_count)
+            pattern = "X*" + "#" + "X+".join([g_group_size] * 4) + "#|" + "X*"
+            pattern = pattern.replace("X", x)
+            select_parts_pattern_for_dilute = random.sample(range(0, 4), k=3)
+            while True:
+                select_parts_pattern_for_dilute = [random.randint(0, 1) for _ in range(0, 4)]
+                if not all(select_parts_pattern_for_dilute):
+                    break
+
+            parts_pattern = pattern.split('+', -1)
+            pattern = '+'.join([part.replace('Y*', y) if
+                                select_parts_pattern_for_dilute[idx] else part.replace('Y*', '') for idx, part in
+                                enumerate(parts_pattern)])
+            return rstr.xeger(pattern)
+
+        gqd_strings = [gqd_random_string(n) for n in generator]
+        gqd_strings_strength = [dafna.max_gquadruplex_strength(gqd) for gqd in gqd_strings]
+        pprint.pprint(dict(zip(gqd_strings,  gqd_strings_strength)), sort_dicts=False)
+        self.assertEqual(list(generator), gqd_strings_strength)
