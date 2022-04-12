fe5202e2350f2fe67467962de62cec0aefeca775
Ilya Slynko <ilya.slynko1@gmail.com>
2022-04-12 15:38:35 +0300

    fix imotif max strength(no pull)

--- test/test_i_motif.py
+++ test/test_i_motif.py
@@ -1,16 +1,17 @@
+import pprint
+
 import regex
 import unittest
 import rstr
-import pprint
 import dafna
 
 
 class TestIMotif(unittest.TestCase):
     def test_01(self):
-        generator = range(2, 21)
+        generator = range(3, 20)
 
         def i_motif_random_string(n, m, biological_significance=False):
-            x_group = '(a|c|g|t)'
+            x_group = '(a|g|t)'
             y_symbol = '(t|c)'
             i_motif_form = [
                 '{Y}c{{{n}}}g{X}{{2}}{Y}c{{{m}}}g{X}{{1}}{Y}c{{{n}}}g{X}{{2}}{Y}c{{{m}}}g',
@@ -22,38 +23,49 @@ class TestIMotif(unittest.TestCase):
             return rstr.xeger(pattern)
 
         def i_motif_max_strength(string, biological_significance: bool = False):
+            x_group = '[a|c|g|t]'
+            y_symbol = '[t|c]'
+            n = '3,'
+            m = '1,'
+            biological_significance_pattern = '((c{{{n}}}){X}{{3,}}?(c{{{m}}}){X}{{3,}}?()\\2{X}{{3,}}?()\\3)'
+            no_biological_significance_pattern = '({Y}(c{{1,}})g{X}{{2}}{Y}(c{{1,}})g{X}{{1}}{Y}()\\2g{X}{{2}}{Y}()\\3g)'
 
-            def get_i_motif_pattern(biological_significance: bool):
-                x_group = '[a|c|g|t]'
-                y_symbol = '[t|c]'
-                patterns = [
-                    [
-                        '({Y}(c{{1,}})g{X}{{2}}{Y}(c{{1,}})g{X}{{1,}}?{Y}()\\2g{X}{{2}}{Y}()\\3g)',
-                        '({Y}(c{{1,}})g{X}{{2}}{Y}(c{{1,}}+)g{X}{{1,}}?{Y}()\\2g{X}{{2}}{Y}()\\3g)'
-                    ],
-                    [
-                        '((c{{3,}}){X}{{3,}}?(c{{1,}}+){X}{{3,}}?()\\2{X}{{3,}}?()\\3)',
-                        '((c{{3,}}){X}{{3,}}?(c{{1,}}){X}{{3,}}?()\\2{X}{{3,}}?()\\3)'
-                    ]
+            if biological_significance:
+                pattern = biological_significance_pattern.format(Y=y_symbol, X=x_group, n=n, m=m)
+                match = regex.findall(pattern, string)
+                if any(match):
+                    strength = 0
+                    for i_motif in match:
+                        sub_strength = (len(i_motif[1]) + len(i_motif[2])) / 2
+                        for i in i_motif:
+                            sub_match = regex.findall(
+                                biological_significance_pattern.format(Y=y_symbol, X=x_group, n=i, m=m), string)
+                            if any(sub_match):
+                                temp_strength = (sub_match[1] + sub_match[2]) / 2
+                                sub_strength = sub_strength if sub_strength > temp_strength else temp_strength
 
-                ]
-                return [pat.format(Y=y_symbol, X=x_group) for pat in patterns[bool(biological_significance)]]
+                        strength = strength if strength > sub_strength else sub_strength
+                    return strength
 
-            pattern = get_i_motif_pattern(biological_significance)
-            match = [regex.findall(pat, string) for pat in pattern]
-            if match:
-                return max([(len(mat[1])+len(mat[2]))/2 for pair_mat in match for mat in pair_mat])
+            else:
+                pattern = no_biological_significance_pattern.format(Y=y_symbol, X=x_group)
+                match = regex.findall(pattern, string)
+                if any(match):
+                    strength = 0
+                    for i_motif in match:
+                        sub_strength = (len(i_motif[1]) + len(i_motif[2])) / 2
+                        strength = strength if strength > sub_strength else sub_strength
+                    return strength
             return 0
 
-        # a = [i_motif_random_string(n, n + 1) for n in range(2, 6)]
-        b = [i_motif_random_string(n, n+2, True) for n in range(3, 10)]
-        pprint.pprint(b)
-        pprint.pprint(
-            [dafna.strength.max_imotiv_stength(bi) for bi in b]
-        )
-        i_motif_max_strength('cccctgcccctaccccgtacccc', True)
+        biological_significance_strings = [i_motif_random_string(n, n + 2, True) for n in generator]
+        no_biological_significance_strings = [i_motif_random_string(n, n + 2, False) for n in generator]
 
-        # self.assertEqual(list(generator), gqd_strings_strength)
+        self.assertEqual([(n + n + 2) / 2 for n in list(generator)],
+                         [i_motif_max_strength(i_motif, True) for i_motif in biological_significance_strings])
+        self.assertEqual([(n + n + 2) / 2 for n in list(generator)],
+                         [i_motif_max_strength(i_motif, False) for i_motif in no_biological_significance_strings])
+        self.assertEqual(6, i_motif_max_strength('cccccctacccccccccccccccgttccccccc', True))
 
     def test_02(self):
         generator = [2, 3, 4, 5]
