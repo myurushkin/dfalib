715914d89af4a431880e91e45976ac53033a6ae6
Ilya Slynko <ilya.slynko1@gmail.com>
2022-04-18 16:37:04 +0300

    added hairpin strength(no pull)

--- test/test_hairpin.py
+++ test/test_hairpin.py
@@ -12,8 +12,10 @@ class TestHairpin(unittest.TestCase):
         generator = range(2, 21)
 
         def hairpin_random_string(strength):
-            n = random.randint(1, strength)
+            n = random.randint(1, strength-1)
             m = strength - n
+
+            assert m+n == strength
 
             def replace_complimentary_symbol(string):
                 repl = {'a': 't', 't': 'a', 'g': 'c', 'c': 'g'}
@@ -21,16 +23,16 @@ class TestHairpin(unittest.TestCase):
                 pattern = regex.compile("|".join(replacement.keys()))
                 return "".join(list(pattern.sub(lambda m: replacement[regex.escape(m.group(0))], string)))
 
-            hairpin_head_pattern = f'(((a|t){{{n}}})(a|t|g|c){{1,3}}((g|c){{{m}}}))'
+            hairpin_head_pattern = f'((a|t){{{n}}})(a|t|g|c){{0,3}}((g|c){{{m}}})'
             hairpin_loop_pattern = '(a|t){3,7}'
-            hairpin_tail_pattern = '({gc_group}(a|c|g|t){{1,3}}{at_group})'
+            hairpin_tail_pattern = '({gc_group}(a|c|g|t){{0,3}}{at_group})'
 
             hairpin_head = rstr.xeger(hairpin_head_pattern)
             hairpin_loop = rstr.xeger(hairpin_loop_pattern)
             sub_groups = regex.findall(hairpin_head_pattern, hairpin_head)[0]
             hairpin_tail = rstr.xeger(hairpin_tail_pattern.format(
-                gc_group=replace_complimentary_symbol(sub_groups[4][::-1]),
-                at_group=replace_complimentary_symbol(sub_groups[1][::-1])
+                gc_group=replace_complimentary_symbol(sub_groups[3][::-1]),
+                at_group=replace_complimentary_symbol(sub_groups[0][::-1])
             )
             )
 
@@ -46,46 +48,41 @@ class TestHairpin(unittest.TestCase):
             n_count = '1,'
             m_count = '1,'
             pattern = \
-                '(({at_group})[a|t|g|c]{{1,3}}({gc_group})[a|t|g|c]{{3,7}}({gc_complimentary_group})[a|t|g|c]{{1,3}}({at_complimentary_group}))'
-            start_pattern = '(([a|t]{{{n_count}}})[a|t|g|c]{{1,3}}([g|c]{{{m_count}}}))[a|t|g|c]{{3,7}}([g|c]{{1,}})'
-            match = regex.findall(start_pattern.format(n_count=n_count, m_count=m_count), string)
+                '({at_group})[a|t|g|c]{{0,3}}?({gc_group})[a|t|g|c]{{3,7}}({gc_complimentary_group})[a|t|g|c]{{0,3}}({at_complimentary_group})'
+            start_pattern = '([a|t]{{{n_count}}})[a|t|g|c]{{{bubble_count}}}([g|c]{{{m_count}}})'
+            match = []
+            for i in range(0,4):
+                match += regex.findall(start_pattern.format(n_count=n_count, m_count=m_count, bubble_count=i), string)
             if any(match):
                 for hairpin_head_part in match:
-                    sub_strength = len(hairpin_head_part[1]) + len(hairpin_head_part[2])
-                    n = len(hairpin_head_part[1])
+                    n = len(hairpin_head_part[0])
                     for i in range(n, 0, -1):
-                        sub_match = regex.findall(start_pattern.format(n_count=i, m_count=m_count), string)
-                        if any(sub_match):
-                            at_group, at_complimentary_group = sub_match[0][1], replace_complimentary_symbol(sub_match[0][1])
-                            gc_group, gc_complimentary_group = sub_match[0][2], replace_complimentary_symbol(sub_match[0][2][::-1])
-                            full_match = regex.findall(pattern.format(
-                                    gc_group=gc_group,
-                                    gc_complimentary_group=gc_complimentary_group,
-                                    at_group=at_group,
-                                    at_complimentary_group=at_complimentary_group
-                                ),
-                                string
-                            )
+                        for bubble_count in range(0,4):
+                            sub_match = regex.findall(start_pattern.format(n_count=i, m_count=m_count, bubble_count=bubble_count), string)
+                            if any(sub_match):
+                                at_group, at_complimentary_group = sub_match[0][0], replace_complimentary_symbol(sub_match[0][0][::-1])
+                                gc_group, gc_complimentary_group = sub_match[0][1], replace_complimentary_symbol(sub_match[0][1][::-1])
+                                a = pattern.format(
+                                        gc_group=gc_group,
+                                        gc_complimentary_group=gc_complimentary_group,
+                                        at_group=at_group,
+                                        at_complimentary_group=at_complimentary_group
+                                    )
+                                full_match = regex.findall(pattern.format(
+                                        gc_group=gc_group,
+                                        gc_complimentary_group=gc_complimentary_group,
+                                        at_group=at_group,
+                                        at_complimentary_group=at_complimentary_group
+                                    ),
+                                    string
+                                )
 
-                            if any(full_match):
-                                temp_strength = len(full_match[0][1]) + len(full_match[0][2])
-                                sub_strength = sub_strength if sub_strength > temp_strength else temp_strength
-
-                    strength = strength if strength > sub_strength else sub_strength
+                                if any(full_match):
+                                    for match_case in full_match:
+                                        temp_strength = len(match_case[0]) + len(match_case[2])
+                                        strength = strength if strength > temp_strength else temp_strength
             return strength
-        print(hairpin_random_string(3))
-        print(hairpin_max_strength(hairpin_random_string(3)))
-
-    def test_02(self):
-        generator = [2, 3, 4, 5]
-
-        gqd_strings = [
-            'accattcacccgggaaggtagggcaaactccattcacccttctggaacacctcaacaattattactactactatatccggggcaaactaaaccctttaacctccactctgtttacaatccaaaaatctattctctatcctcgaacgcttcctacacatacaattatttcggctttatcccaccataactccttcttcattcatttgagctaccttcaatttatacattaacatccctcactcctgactatcacaagaacacattcaccaggatacaattcataaagaacttccccttatcatccagtctcactacaa',
-            'cttccacataaactatggtacacttttttttttctcttaaagggtcaaacccccaaaaatccttctccgaaaaaaggctacttggcaaaaccccaaccccattttctccatcccatataactactagttctgatgctaaccactcacacacccaatatatttccaaacttcaatcactctttttcacctaagggtttcctatcccaaattacacttcctttgggactaccctatattaaaattccaactcccacccatcatcaccactaatattccc',
-            'ctcttttcattctacagggtctctatcacataacctcttattttcttataccattccttacccgctcaggtgcaatccattcatacaaaaatttcaacacttcaaactctggggacactctacactactttacacaactccttataccatcacatctcatttccacctattcctatttcattattccattatataattacaccaccaggggacactctacccaattcctttgctcctcctctacccaaaaagtttacttctcaacaggggtcatcctcatcagatataac',
-            'acccacatttactgacaacaactcccaacttgtgagtctccattcaatcttataaatatcaaatatcaaatataatcaaaacttaatcctttcagggggcccaatatatataatgatgacagtatcgtccgcccaactttaacactcacttccaatatcaatcttcattcccctaattcaccccctcactatacttaattagtgctgtactgctgtacttctttctccgttataaaacctttataaccgcttcataaaactatcccaatatttatatcatttctaacccctaacttaatcctaattttttagcgcactgtaagttgaaaactactacttatttatcctaaaacactttcacaacacccaacctttctctcttttatcactcttaagatcacattctagactcccctttca',
-        ]
-
-        gqd_strings_strength = [dafna.gqd_max_strength(gqd) for gqd in gqd_strings]
-        # pprint.pprint(dict(zip(gqd_strings,  gqd_strings_strength)), sort_dicts=False)
-        self.assertEqual(generator, gqd_strings_strength)
+        a = hairpin_random_string(6)
+        print(a)
+        # print(hairpin_max_strength(a))
+        print(hairpin_max_strength('ttttgggaaattatccgaaaaa'))
